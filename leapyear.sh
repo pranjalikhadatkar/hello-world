95458a63ae1edf87406c9360f9c7acf765799710
pranjalikhadatkar <94820061+pranjalikhadatkar@users.noreply.github.com>
2021-11-22 11:18:28 +0530

    added leapyear.sh file

---
+++ leapyear.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -n "Enter year(yyyy) : "
+read y
+
+a=$y%4
+b=$y%100
+c=$y%400
+
+if [[ $a -eq 0 || ( $b -eq 0 && !( $c -eq 0 ) ) ]]
+then
+	echo "$y is leap year"
+else
+	echo "$y is not leap year"
+fi
